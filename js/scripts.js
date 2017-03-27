d678b566e33b624439051a3703c6dacf0f28a0bc
RomainGuilmet <romain.guilmet@teads.tv>
2017-03-27 13:39:11 +0200

    add a dropDown menu

--- js/scripts.js
+++ js/scripts.js
@@ -399,4 +399,14 @@
         document.getElementsByClassName('home-bottom')[0].style.bottom = 0
     }
 
+    var timerIn = 200;
+    var timerOut = 200;
+    $('.dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).fadeIn(timerIn);
+        $(this).addClass('open');
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).fadeOut(timerOut);
+        $(this).removeClass('open');
+    });
+
 })(jQuery);
