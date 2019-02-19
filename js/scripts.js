63a8b8c0ae67eb2334716317a108c42af25ff25b
RonanDrouglazet <ronan.drouglazet@teads.tv>
2019-02-19 22:48:57 +0100

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -46,7 +46,7 @@
         /* -------------------
         Back to top button popup
         ---------------------*/
-        if ($(window).scrollTop() > $('#lasalle').offset().top){
+        if ($(window).scrollTop() > ($('#lasalle').length ? $('#lasalle').offset().top : 0)){
             $("#back-to-top").stop().animate({ bottom:'30px' },300,'easeInOutCubic')
         }
         else{
